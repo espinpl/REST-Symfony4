a9a04caaf3829d3e4e73bc7b1ed5d107c3cb9a5e
espin.pl <g.dachowski@gmail.com>
2019-01-27 16:07:39 +0100

    update RestController.php file

--- src/Controller/RestController.php
+++ src/Controller/RestController.php
@@ -31,29 +31,29 @@ class RestController extends AbstractController {
     { 
         $data = json_decode($request->get('user'));
 			
-		if (empty($data)) { 
-			return new JsonResponse(['error' => 'no content']);
-		}
+	    if (empty($data)) { 
+	        return new JsonResponse(['error' => 'no content']);
+        }
 		
-			try 
-			{ 
-			    $entityManager = $this->getDoctrine()->getManager();
-			    $users = new Users();
-			    $users->setUsername($data->username);
-			    $users->setPassword($data->password);		
-			    $users->setEmail($data->email);	
-			    $users->setFirstname($data->firstname);	
-			    $users->setLastname($data->lastname);	
-			    $users->setStatus($data->status);	
-                $entityManager->persist($users);
-                $entityManager->flush();
+	    try 
+	    { 
+	        $entityManager = $this->getDoctrine()->getManager();
+	        $users = new Users();
+	        $users->setUsername($data->username);
+	        $users->setPassword($data->password);		
+	        $users->setEmail($data->email);	
+	        $users->setFirstname($data->firstname);	
+	        $users->setLastname($data->lastname);	
+	        $users->setStatus($data->status);	
+            $entityManager->persist($users);
+            $entityManager->flush();
 
-			} catch (\Exception $m) 
-			{
-				return new JsonResponse(['error' => $m->getMessage()], JsonResponse::HTTP_BAD_REQUEST);
-			} 
+	    } catch (\Exception $m) 
+	    {
+	        return new JsonResponse(['error' => $m->getMessage()], JsonResponse::HTTP_BAD_REQUEST);
+	    } 
 		
-		return new JsonResponse(['message' => 'The user was created.', 'user' => $data], JsonResponse::HTTP_CREATED);
+	    return new JsonResponse(['message' => 'The user was created.', 'user' => $data], JsonResponse::HTTP_CREATED);
     }
 
     /**
@@ -62,12 +62,12 @@ class RestController extends AbstractController {
     public function Read($id, Request $request, SerializerInterface $serializer)
     { 
         try 
-		{
+	    {
             $user = $this->repository->findOneById($id);
 			
-			if (!$user) {
+	        if (!$user) {
                 return new JsonResponse(['error' => 'No user found.']);
-			}
+	        }
 			
             $jsonContent = $serializer->serialize($user, 'json');
 
@@ -76,10 +76,10 @@ class RestController extends AbstractController {
 
             return $response;
 
-		} catch (\Exception $m) 
-		{
-			return new JsonResponse(['error' => $m->getMessage()], JsonResponse::HTTP_BAD_REQUEST);
-		}
+	    } catch (\Exception $m) 
+	    {
+	        return new JsonResponse(['error' => $m->getMessage()], JsonResponse::HTTP_BAD_REQUEST);
+	    }
     }
 	
     /**
@@ -87,35 +87,35 @@ class RestController extends AbstractController {
      */
     public function Update($id, Request $request)
     { 
-		$data = json_decode($request->getContent());
+	    $data = json_decode($request->getContent());
 		
-		if (empty($data)) { 
-			return new JsonResponse(['error' => 'no content']);
-		}
+	    if (empty($data)) { 
+	        return new JsonResponse(['error' => 'no content']);
+	    }
 		
-		$user = $this->repository->findOneById($id);
+	    $user = $this->repository->findOneById($id);
 		
-		if (!$user) {
-			return new JsonResponse(['error' => 'No user found.']);
-		}		
+	    if (!$user) {
+	        return new JsonResponse(['error' => 'No user found.']);
+	    }		
 		
-			try 
-			{ 
-				$entityManager = $this->getDoctrine()->getManager();
-			    $user->setUsername($data->username);
-			    $user->setPassword($data->password);		
-			    $user->setEmail($data->email);	
-			    $user->setFirstname($data->firstname);	
-			    $user->setLastname($data->lastname);	
-			    $user->setStatus($data->status);				
-                $entityManager->flush();
+	    try 
+	    { 
+	        $entityManager = $this->getDoctrine()->getManager();
+	        $user->setUsername($data->username);
+            $user->setPassword($data->password);		
+	        $user->setEmail($data->email);	
+	        $user->setFirstname($data->firstname);	
+	        $user->setLastname($data->lastname);	
+	        $user->setStatus($data->status);				
+            $entityManager->flush();
 			
-			} catch (\Exception $m) 
-			{
-				return new JsonResponse(['error' => $m->getMessage()], JsonResponse::HTTP_BAD_REQUEST);
-			} 
-			
-		return new JsonResponse(['message' => 'The user was updated.', 'user' => $data], JsonResponse::HTTP_OK);
+	    } catch (\Exception $m) 
+	    {
+	        return new JsonResponse(['error' => $m->getMessage()], JsonResponse::HTTP_BAD_REQUEST);
+        } 
+		
+	    return new JsonResponse(['message' => 'The user was updated.', 'user' => $data], JsonResponse::HTTP_OK);
 	}
 	
     /**
@@ -123,24 +123,24 @@ class RestController extends AbstractController {
      */
     public function Delete($id)
     { 
-		$user = $this->repository->findOneById($id);
+	    $user = $this->repository->findOneById($id);
 		
-		if (!$user) {
-			return new JsonResponse(['error' => 'No user found.']);
-		}
+	    if (!$user) {
+	        return new JsonResponse(['error' => 'No user found.']);
+        }
 		
-		try 
-		{ 
-			$entityManager = $this->getDoctrine()->getManager();
-			$entityManager->remove($user);
-			$entityManager->flush();
+	    try 
+	    { 
+	        $entityManager = $this->getDoctrine()->getManager();
+	        $entityManager->remove($user);
+	        $entityManager->flush();
 			
-			return new JsonResponse(['message' => 'The user was deleted.'], JsonResponse::HTTP_OK);
+	        return new JsonResponse(['message' => 'The user was deleted.'], JsonResponse::HTTP_OK);
 			
-		} catch (\Exception $m) 
-		{
-			return new JsonResponse(['error' => $m->getMessage()], JsonResponse::HTTP_BAD_REQUEST);
-		}		
-	}
+        } catch (\Exception $m) 
+	    {
+	        return new JsonResponse(['error' => $m->getMessage()], JsonResponse::HTTP_BAD_REQUEST);
+	    }		
+    }
 	
 }
